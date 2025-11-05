47c2dea280c18fd54ec76031bbe98b3b644febff
심은주 <nyaknya@naver.com>
2025-11-05 12:59:12 +0900

    ✨ feat: 위프코 추가

--- components/DevProject/index.tsx
+++ components/DevProject/index.tsx
@@ -67,8 +67,12 @@ export default function DevProject() {
           <h2 ref={titleRef} className="text-4xl sm:text-5xl font-bold">
             Dev Projects
           </h2>
-          <p ref={subtitleRef} className="text-base sm:text-2xl font-bold flex flex-col lg:flex-row  gap-2 lg:gap-1">
-            지난 3년간 100개 이상의<br className="block sm:hidden" /> 프로젝트를 제작했습니다.{" "}
+          <p
+            ref={subtitleRef}
+            className="text-base sm:text-2xl font-bold flex flex-col lg:flex-row  gap-2 lg:gap-1"
+          >
+            지난 3년간 100개 이상의
+            <br className="block sm:hidden" /> 프로젝트를 제작했습니다.{" "}
             <Link
               href={
                 "https://diamond-nerve-9e2.notion.site/44dc3f3a136a463e9a9152f4577c15e9?source=copy_link"
@@ -374,7 +378,7 @@ export default function DevProject() {
                   PHOTOSHOP
                 </li>
               </ul>
-               <ul className="text-xl font-bold flex flex-col sm:flex-row gap-6 sm:gap-10">
+              <ul className="text-xl font-bold flex flex-col sm:flex-row gap-6 sm:gap-10">
                 <li>
                   <Link
                     href={"/img/alssahan1.png"}
@@ -424,7 +428,7 @@ export default function DevProject() {
             ref={(el) => {
               projectItemsRef.current[4] = el;
             }}
-               className="p-4 py-8 lg:p-12 border-b flex flex-col lg:flex-row lg:items-center gap-5 lg:gap-20"
+            className="p-4 py-8 lg:p-12 border-b flex flex-col lg:flex-row lg:items-center gap-5 lg:gap-20"
           >
             <figure>
               <Image
@@ -482,6 +486,91 @@ export default function DevProject() {
                 <li>
                   <Link
                     href={"https://sehwaetc.com/default/"}
+                    target="_blank"
+                    className="flex gap-4 items-center"
+                  >
+                    <Image
+                      src="/img/site_link.png"
+                      alt="figma"
+                      width={28}
+                      height={28}
+                    />
+                    배포 링크
+                    <Image
+                      src={"/img/more_link_b.png"}
+                      alt="링크 이동"
+                      width={14}
+                      height={14}
+                    />
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </li>
+          <li
+            ref={(el) => {
+              projectItemsRef.current[4] = el;
+            }}
+            className="p-4 py-8 lg:p-12 border-b flex flex-col lg:flex-row lg:items-center gap-5 lg:gap-20"
+          >
+            <figure>
+              <Image
+                src={"/img/wipco.png"}
+                width={460}
+                height={365}
+                alt="fitmon"
+              />
+            </figure>
+            <div className="flex-col flex gap-6">
+              <h3 className="text-3xl sm:text-4xl items-center flex gap-4 sm:gap-6">
+                <span className="text-2xl font-bold">06</span>
+                <strong>WIPCO</strong>
+              </h3>
+              <p className="text-base sm:text-lg leading-8">
+                AI 3D 재구성 디지털 트윈 전문 업체 위프코
+                <br />
+                <strong>디자인 및 퍼블리싱 기여도 100%</strong> | 디자인
+                소요기간 1~2일 | 퍼블리싱 3일
+              </p>
+              <ul className="flex flex-wrap gap-2">
+                <li className="px-5 py-2 rounded-3xl border border-gray-300 bg-black text-white font-semibold">
+                  그누보드
+                </li>
+                <li className="px-5 py-2 rounded-3xl border border-gray-300 font-semibold">
+                  GSAP
+                </li>
+                <li className="px-5 py-2 rounded-3xl border border-gray-300 font-semibold">
+                  HTML/CSS/JS
+                </li>
+                <li className="px-5 py-2 rounded-3xl border border-gray-300 font-semibold">
+                  PHOTOSHOP
+                </li>
+              </ul>
+              <ul className="text-xl font-bold flex flex-col sm:flex-row gap-6 sm:gap-10">
+                <li>
+                  <Link
+                    href={"/img/wipco1.png"}
+                    className="flex gap-4 items-center"
+                    target="_blank"
+                  >
+                    <Image
+                      src="/img/ps-icon.png"
+                      alt="figma"
+                      width={24}
+                      height={32}
+                    />
+                    디자인 상세 링크
+                    <Image
+                      src={"/img/more_link_b.png"}
+                      alt="링크 이동"
+                      width={14}
+                      height={14}
+                    />
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    href={"https://wipco.co.kr/"}
                     target="_blank"
                     className="flex gap-4 items-center"
                   >
